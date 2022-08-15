7eb37632d33c5436f52b5f5917d097e2c5141e81
liang <1968971482@qq.com>
2022-08-15 23:30:54 +0800

    项目初始化

---
+++ src/main/java/com/example/testgit04/TestGit04Application.java
@@ -0,0 +1,13 @@
+package com.example.testgit04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGit04Application.class, args);
+    }
+
+}
