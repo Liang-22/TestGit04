d963fdb19e213906b143fc402ef512321ee9632f
liang <1968971482@qq.com>
2022-08-15 23:31:45 +0800

    项目初始化001

---
+++ src/main/java/com/example/testgit04/TesstGit01.java
@@ -0,0 +1,13 @@
+package com.example.testgit04;
+
+/**
+ * @Author: Liang
+ * @CreateTime: 2022-08-15 23:31
+ * @Description:
+ */
+public class TesstGit01 {
+    public static void main(String[] args) {
+        System.out.println("001");
+    }
+
+}
