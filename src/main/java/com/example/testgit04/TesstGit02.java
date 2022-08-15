c275afe5c1edfe615b0509a07a87076dcf607212
liang <1968971482@qq.com>
2022-08-15 23:32:20 +0800

    项目初始化002

---
+++ src/main/java/com/example/testgit04/TesstGit02.java
@@ -0,0 +1,12 @@
+package com.example.testgit04;
+
+/**
+ * @Author: Liang
+ * @CreateTime: 2022-08-15 23:31
+ * @Description:
+ */
+public class TesstGit02 {
+    public static void main(String[] args) {
+        System.out.println("002");
+    }
+}
